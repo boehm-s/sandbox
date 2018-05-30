3574d4b601ac4c6050803899d92859dc27cc3e69
Steven BOEHM <boehm_s@etna-alternance.net>
2018-05-30 23:49:00 +0200

    lib working fine, will start to get some stats about my music

--- elixir/mp3Edit/lib/mp3Edit.ex
+++ elixir/mp3Edit/lib/mp3Edit.ex
@@ -1,11 +1,10 @@
-defmodule Mp3Edit do
-  contents = File.read("./mp3/nested/07 Talk On The Street.mp3")
-  # tag_header = ID3v2.header(contents)
-  # {major, minor} = tag_header.version
-  # IO.puts "ID3 version 2.#{major}.#{minor}"
+defmodule MusicLib do
+  {:ok, contents} = File.read("./mp3/nested/07 Talk On The Street.mp3")
+  tag_header = ID3v2.header(contents)
+  {major, minor} = tag_header.version
+  IO.puts "ID3 version 2.#{major}.#{minor}"
 
   result = ID3v2.frames(contents)
-  IO.puts "Track title: #{result.TIT2}"
-  IO.puts "Track artist: #{result.TPE1}"
-  IO.puts "Track album: #{result.TALB}"
+  IO.inspect result
+  IO.puts "Track : #{result["TIT2"]}"
 end
